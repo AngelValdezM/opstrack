bf33ab0f2c04d2ff97d0d9401afc22fd65deb7fb
Angel Valdez <angelvm_2004@hotmail.com>
2026-09-11 21:25:57 -0500

    feat: initial flask app with home and employees routes

--- app.py
+++ app.py
@@ -1,1 +1,35 @@
-print("Hola Mundo")
+from flask import Flask,jsonify
+
+app = Flask(__name__)
+
+@app.route("/")
+def inicio():
+    # tu código aquí — retorna un diccionario, Flask lo convierte a JSON solo
+    return {"mensaje": "Hola"}
+    
+
+@app.route("/empleados")
+def obtener_empleados():
+    lista_empleados = [
+        {
+            "id": 1,
+            "nombre": "Angel",
+            "cargo": "Ingeniero"
+        },
+        {
+            "id": 2,
+            "nombre": "Christian",
+            "cargo": "Ingeniero"
+        },
+        {
+            "id": 3,
+            "nombre": "Jose Luis",
+            "cargo": "Ingeniero"
+        }
+    ]
+    return jsonify(lista_empleados)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
