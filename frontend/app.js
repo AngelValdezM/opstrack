dedaf217265bdfe9c8d94d83108d36538bad73c2
Angel Valdez <angelvm_2004@hotmail.com>
2026-09-19 00:19:58 -0500

    feat: replace submit event with new edit function

--- frontend/app.js
+++ frontend/app.js
@@ -22,55 +22,6 @@ async function cargarEmpleados() {
 cargarEmpleados();
 
 document.getElementById("form-empleado").addEventListener("submit", async (evento) => {
-    evento.preventDefault(); // evita que la página se recargue (comportamiento normal de un <form>)
-
-    const nombre = document.getElementById("input-nombre").value;
-    const cargo = document.getElementById("input-cargo").value;
-    
-    const respuesta = await fetch("http://127.0.0.1:5000/empleados", {
-                            method: "POST",
-                            headers: { "Content-Type": "application/json" },
-                            body: JSON.stringify({ nombre: nombre, cargo: cargo })
-                        });
-    
-    if (!respuesta.ok) {
-    console.error("Error al crear empleado");
-    return; 
-    }
-    
-    const contenedor = document.getElementById("tabla-empleados");
-    contenedor.innerHTML = "";
-
-    cargarEmpleados();
-    
-    document.getElementById("input-nombre").value = "";
-    document.getElementById("input-cargo").value = "";
-
-});
-
-async function eliminarEmpleado(id) {
-    const validacion = confirm("¿Seguro que quieres eliminar este empleado?");
-    if(!validacion) {
-        return;
-    } else {
-        const respuesta = await fetch(`http://127.0.0.1:5000/empleados/${id}`, { method: "DELETE" });
-        const contenedor = document.getElementById("tabla-empleados");
-        contenedor.innerHTML = "";
-        cargarEmpleados();
-    }
-}
-
-function prepararEdicion(id, nombre, cargo) {
-    document.getElementById("input-id-editar").value = id;
-    document.getElementById("input-nombre").value = nombre;
-    document.getElementById("input-cargo").value = cargo;
-    
-    const boton = document.querySelector("#form-empleado button[type='submit']");
-    boton.textContent = "Actualizar empleado";
-
-}
-
-document.getElementById("form-empleado").addEventListener("submit", async (evento) => {
     evento.preventDefault();
 
     const nombre = document.getElementById("input-nombre").value;
@@ -105,3 +56,26 @@ document.getElementById("form-empleado").addEventListener("submit", async (event
     contenedor.innerHTML = "";
     cargarEmpleados();
 });
+
+async function eliminarEmpleado(id) {
+    const validacion = confirm("¿Seguro que quieres eliminar este empleado?");
+    if(!validacion) {
+        return;
+    } else {
+        const respuesta = await fetch(`http://127.0.0.1:5000/empleados/${id}`, { method: "DELETE" });
+        const contenedor = document.getElementById("tabla-empleados");
+        contenedor.innerHTML = "";
+        cargarEmpleados();
+    }
+}
+
+function prepararEdicion(id, nombre, cargo) {
+    document.getElementById("input-id-editar").value = id;
+    document.getElementById("input-nombre").value = nombre;
+    document.getElementById("input-cargo").value = cargo;
+    
+    const boton = document.querySelector("#form-empleado button[type='submit']");
+    boton.textContent = "Actualizar empleado";
+
+}
+
